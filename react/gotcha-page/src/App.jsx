f051475b2c252996fa0317c8a69f091baafc0030
Mmmr7 <ol-sidorenkov@mail.ru>
2022-05-14 18:01:27 +0300

    Rename

---
+++ react/gotcha-page/src/App.jsx
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [nicknameInputVal, setNicknameInputVal] = useState("")
+  const onInput = (e) => {
+    setNicknameInputVal(e.target.value)
+  }
+  return (
+      <>
+        {nicknameInputVal}
+        <br/>
+        <input value={nicknameInputVal} onChange={onInput}/>
+      </>
+  );
+}
+
+export default App;
