cfc2266a8b51502727121c5b31f5750f44572644
Mmmr7 <ol-sidorenkov@mail.ru>
2022-05-29 03:09:06 +0300

    Redo architecture

--- scripts/selectAll.js
+++
@@ -1,9 +0,0 @@
-function selectAll(source) {
-    $(".ticks")
-        .find("input[type=checkbox], select")
-        .each(
-            function () {
-                this.checked = source.checked
-            }
-        )
-}
