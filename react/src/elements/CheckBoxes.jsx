90fd21ca1a50a2aeeb78370f8dd48410043b3475
Mmmr7 <ol-sidorenkov@mail.ru>
2022-06-16 00:34:09 +0300

    checkboxes

--- react/src/elements/CheckBoxes.jsx
+++ react/src/elements/CheckBoxes.jsx
@@ -3,16 +3,29 @@ import axios from 'axios';
 import {useEffect, useState} from "react";
 
 function CheckBoxes() {
-    const [checkboxes, setCheckboxes] = useState();
+    const [checkboxes, setCheckboxes] = useState([]);
     useEffect(() => {
         axios.get("http://localhost:8080")
             .then((resp) => {
                 setCheckboxes(resp.data);
+            })
+            .catch((err) => {
+                console.log(err)
             });
     }, []);
+
     return (
         <>
-            {JSON.stringify(checkboxes)}
+            <div className="checks">
+                <ul>
+                    {checkboxes.map(
+                        (x, k) => <li key={k}>
+                            <label form={x.id}>{x.name}</label>
+                            <input type="checkbox" name={x.id} id={x.id}/>
+                        </li>
+                    )}
+                </ul>
+            </div>
         </>
     );
 }
